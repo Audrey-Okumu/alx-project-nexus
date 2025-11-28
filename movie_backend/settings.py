df53f7fcfd75d1fc6d4c653489e5f604dd1fce5c
Audrey <audreyokumu@gmail.com>
2025-11-28 13:06:33 +0300

    Fix Redis configuration and add database services to render.yaml

--- movie_backend/settings.py
+++ movie_backend/settings.py
@@ -161,7 +161,7 @@ REDIS_URL = os.getenv('REDIS_URL', 'redis://127.0.0.1:6379/1')
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/1",
+        "LOCATION": REDIS_URL,
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         }
