baf9a08b8963f194df857ef68fdc75c213a85b5c
Audrey <audreyokumu@gmail.com>
2025-11-30 10:16:01 +0300

    fix: removerd deployment actions and set back to development

--- movie_backend/wsgi.py
+++ movie_backend/wsgi.py
@@ -9,12 +9,7 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 
 import os
 from django.core.wsgi import get_wsgi_application
-from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'movie_backend.settings')
 
 application = get_wsgi_application()
-
-# Serve static files with WhiteNoise
-application = WhiteNoise(application)
-application.add_files(os.path.join(os.path.dirname(__file__), '..', 'staticfiles'), prefix='/static/')
